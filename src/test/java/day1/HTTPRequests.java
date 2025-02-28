88b445ce663908d1327c74873c7343f0a6d69acb
Nhn2025 <dnhu122025@gmail.com>
2025-02-28 19:19:21 +0700

    HTTPRequests

---
+++ src/test/java/day1/HTTPRequests.java
@@ -0,0 +1,88 @@
+package day1;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+/*
+ given()
+    content type, set cookies, add auth, add param, set headers info etc....
+
+ when()
+    get, post, put, delete
+
+ then()
+    validate status code, extract response, extract headers cookies & response body....
+
+ */
+
+public class HTTPRequests {
+
+    int id;
+
+    @Test(priority = 1)
+    void createUser() {
+        HashMap data = new HashMap();
+        data.put("name", "nhu");
+        data.put("job", "tester");
+
+        id=given()
+                .contentType("application/json")
+                .body(data)
+
+                .when()
+                .post("https://reqres.in/api/users")
+                .jsonPath().getInt("id");
+
+//        .then()
+//                .statusCode(201)
+//                .log().all();
+    }
+
+    @Test(priority = 2, dependsOnMethods = {"createUser"})
+    void getUser() {
+        given()
+
+        .when()
+                .get("https://reqres.in/api/users?page=" + id)
+
+        .then()
+                .statusCode(200)
+                .body("page", equalTo(id))
+                .log().all();
+    }
+
+    @Test(priority = 3, dependsOnMethods = {"createUser"})
+    void updateUser() {
+        HashMap data = new HashMap();
+        data.put("name", "rua");
+        data.put("job", "auto");
+
+        given()
+                .contentType("application/json")
+                .body(data)
+
+        .when()
+                .put("https://reqres.in/api/users/" + id)
+
+        .then()
+                .statusCode(200)
+                .body("name", equalTo("rua"))
+                .log().all();
+    }
+
+    @Test(priority = 4, dependsOnMethods = {"createUser"})
+    void deleteUser() {
+        given()
+
+        .when()
+                .delete("https://reqres.in/api/users/" + id)
+
+        .then()
+                .statusCode(204)
+                .log().all();
+    }
+
+}
---
+++ src/test/java/day1/HTTPRequestsVersion2.java
@@ -0,0 +1,86 @@
+package day1;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import io.restassured.response.Response;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class HTTPRequestsVersion2 {
+
+    private static final String BASE_URL = "https://reqres.in/api/users";  // Tách URL tránh hardcode
+    private int userId;
+
+    @BeforeClass
+    public void setup() {
+        baseURI = BASE_URL;
+    }
+
+    @Test(priority = 1)
+    public void createUser() {
+        Map<String, String> data = Map.of(
+                "name", "nhu",
+                "job", "tester"
+        );
+
+        Response response = given()
+                .contentType("application/json")
+                .body(data)
+                .when()
+                .post()
+                .then()
+                .statusCode(201)
+                .body("name", equalTo("nhu"))
+                .body("job", equalTo("tester"))
+                .log().all()
+                .extract().response();
+
+        // Kiểm tra id hợp lệ
+        userId = response.jsonPath().getInt("id");
+        Assert.assertTrue(userId > 0, "User ID is not valid!");
+    }
+
+    @Test(priority = 2, dependsOnMethods = {"createUser"})
+    public void getUser() {
+        given()
+                .when()
+                .get("?page=" + userId)  // Sửa lại đúng URL
+                .then()
+                .statusCode(200)
+                .body("page", equalTo(userId))
+                .log().all();
+    }
+
+    @Test(priority = 3, dependsOnMethods = {"createUser"})
+    public void updateUser() {
+        Map<String, String> data = Map.of(
+                "name", "rua",
+                "job", "auto"
+        );
+
+        given()
+                .contentType("application/json")
+                .body(data)
+                .when()
+                .put("/" + userId)
+                .then()
+                .statusCode(200)
+                .body("name", equalTo("rua"))
+                .body("job", equalTo("auto"))
+                .log().all();
+    }
+
+    @Test(priority = 4, dependsOnMethods = {"createUser"})
+    public void deleteUser() {
+        given()
+                .when()
+                .delete("/" + userId)
+                .then()
+                .statusCode(204)
+                .log().all();
+    }
+}
